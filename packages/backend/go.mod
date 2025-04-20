b0b808dd392009da7b3bf2a22cebedd266225e59
Eduardo Ricardo <eduwric@gmail.com>
2025-04-20 12:12:13 -0300

    feat: get security headers

--- packages/backend/go.mod
+++ packages/backend/go.mod
@@ -1,6 +1,8 @@
 module github.com/eduwr/fingerprint/backend
 
-go 1.21
+go 1.22.11
+
+toolchain go1.24.2
 
 require (
 	github.com/go-playground/validator/v10 v10.19.0
@@ -13,12 +15,14 @@ require (
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/google/uuid v1.5.0 // indirect
+	github.com/hashicorp/golang-lru v1.0.2 // indirect
 	github.com/klauspost/compress v1.17.0 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mattn/go-runewidth v0.0.15 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/ua-parser/uap-go v0.0.0-20250326155420-f7f5a2f9f5bc // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.51.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
@@ -26,4 +30,5 @@ require (
 	golang.org/x/net v0.21.0 // indirect
 	golang.org/x/sys v0.17.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- packages/backend/handlers/fingerprint.go
+++ packages/backend/handlers/fingerprint.go
@@ -6,15 +6,11 @@ import (
 	"strings"
 
 	"github.com/eduwr/fingerprint/backend/models"
+	"github.com/ua-parser/uap-go/uaparser"
 
 	"github.com/eduwr/fingerprint/backend/lib"
 	"github.com/gofiber/fiber/v2"
 )
-
-type ValidationError struct {
-	Field   string `json:"field"`
-	Message string `json:"message"`
-}
 
 func HandleFingerprint(c *fiber.Ctx) error {
 	var data models.FingerprintData
@@ -49,7 +45,46 @@ func HandleFingerprint(c *fiber.Ctx) error {
 		}
 	}
 
-	log.Printf("Received fingerprint data: %+v", data)
+	parser, err := uaparser.New("./regexes.yaml")
+	if err != nil {
+		println("error getting the parser")
+		log.Fatal(err)
+	}
+	headers := c.GetReqHeaders()
+
+	println("Security Headers:")
+
+	// User Agent
+	client := parser.Parse(headers["User-Agent"][0])
+
+	fmt.Println(client.UserAgent.Family)
+	fmt.Println(client.UserAgent.Major)
+	fmt.Println(client.UserAgent.Minor)
+	fmt.Println(client.UserAgent.Patch)
+	fmt.Println(client.Os.Family)
+	fmt.Println(client.Os.Major)
+	fmt.Println(client.Os.Minor)
+	fmt.Println(client.Os.Patch)
+	fmt.Println(client.Os.PatchMinor)
+	fmt.Println(client.Device.Family)
+
+	// Is Mobile
+	h, hExists := headers["Sec-Ch-Ua-Mobile"]
+	p, pExists := headers["Sec-Ch-Ua-Platform"]
+
+	if hExists {
+		// If the header exists, join its values
+		println("sec-ch-ua-mobile:", strings.Join(h, ", "))
+	} else {
+		println("sec-ch-ua-mobile header not found")
+	}
+
+	if pExists {
+		// If the header exists, join its values
+		println("sec-ch-ua-platform:", strings.Join(p, ", "))
+	} else {
+		println("sec-ch-ua-platform header not found")
+	}
 
 	return c.Status(fiber.StatusOK).JSON(fiber.Map{
 		"message": "Fingerprint data received successfully",
